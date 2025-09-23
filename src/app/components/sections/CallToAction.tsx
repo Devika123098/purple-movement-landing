d58c4f39c991247080697605dcd95c5f0cc4aa99
Umar Al Mukhtar <umar1868807@gmail.com>
2025-09-23 22:30:53 +0530

    fix entities

--- src/app/components/sections/CallToAction.tsx
+++ src/app/components/sections/CallToAction.tsx
@@ -4,11 +4,11 @@ export const CallToAction = () => {
         <div className="w-full flex justify-center items-center gap-14 p-8 mt-32">
             <div className="max-w-[701px] flex flex-col justify-start items-start gap-4">
                 <div className="w-full bg-gradient-to-t from-[#D6A4FF]  to-[#6F00CD] bg-clip-text text-transparent text-4xl font-bold font-montserrat">
-                    This isn't the end.
+                    This isn&apos;t the end.
                 </div>
                 <div className="w-full">
                     <span className="text-white text-xl font-semibold font-montserrat">
-                        It's only the beginning. <br />
+                        It&apos;s only the beginning. <br />
                         The spark has just been ignited — and with every learner, teacher, and changemaker who joins us, it grows into a fire.
                         <br/><br/>
                     </span>
